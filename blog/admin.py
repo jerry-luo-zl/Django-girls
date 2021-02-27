e73eed0e414971365d81dd2c9be7ce843f7d379c
jerry-luo-zl <57428039+jerry-luo-zl@users.noreply.github.com>
2021-02-27 20:00:00 +0800

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ This is the part for the defination of the administration
 '''
 
 from django.contrib import admin
-from models import Post
+from .models import Post
 
 # to make model visible on the admin page, we must register the model
 admin.site.register(models.Post)
